600a5260ca2e31733dcb534ac013f74daa3da33b
lucapicovici <50777241+lucapicovici@users.noreply.github.com>
2020-03-11 21:55:58 +0000

    Initialise empty project

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+    app     = express();
+
+var indexRoutes = require("./routes/index.js");
+
+app.set("view engine", "ejs");
+
+app.use("/", indexRoutes);
+
+app.listen(3000, () => {
+    console.log("shopping-cart server is now listening to port 3000.");
+});
