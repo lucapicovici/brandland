92098dd52038ceb2e7de58f525b8f7bdd3b66694
lucapicovici <50777241+lucapicovici@users.noreply.github.com>
2020-03-16 22:51:33 +0000

    Add some styling to shop cart page

--- routes/index.js
+++ routes/index.js
@@ -30,4 +30,12 @@ router.get("/add-to-cart/:id", function(req, res){
     });
 });
 
+router.get("/shopping-cart", function(req, res){
+    if (req.session.cart.totalQty == 0) {
+        return res.render("shop/shopping-cart", {products: null});
+    }
+    var cart = new Cart(req.session.cart);
+    return res.render("shop/shopping-cart", {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
 module.exports = router;
